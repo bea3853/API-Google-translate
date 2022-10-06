e0d915de7704fe6c2ec641498f16ad2ffcbda4a4
Beatriz Alves <55098795+bea3853@users.noreply.github.com>
2022-10-06 13:42:32 -0300

    teste1

---
+++ translate.js
@@ -0,0 +1,12 @@
+// function googleTranslateElementInit() {
+//     new google.translate.TranslateElement({pageLanguage: 'pt-br'}, 'google_translate_element');
+//   }
+
+  function googleTranslateElementInit() {
+    new google.translate.TranslateElement({pageLanguage: 'pt-br', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+  }
+
+//   function googleTranslateElementInit() {
+//     new google.translate.TranslateElement({pageLanguage: 'pt-br', layout: google.translate.TranslateElement.InlineLayout.HORIZONTAL}, 'google_translate_element');
+//   }
+
